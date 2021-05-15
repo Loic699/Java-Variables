d11d823c2dd306a0922ab5aa706cbd4ce8343be0
Loic699 <loicpimet@gmail.com>
2021-05-15 20:40:11 +0200

    Variables java

---
+++ Film.java
@@ -0,0 +1,17 @@
+package JavaVariables;
+
+public class Film {
+    public static void main(String[] args) {
+        String name = "film d'Indiana Jones";
+        boolean read = true;
+        int annee = 1989;
+        double rating = 8.2;
+
+        System.out.println(name);
+        System.out.println(read);
+        System.out.println(annee);
+        System.out.println(rating);
+
+    }
+
+}
